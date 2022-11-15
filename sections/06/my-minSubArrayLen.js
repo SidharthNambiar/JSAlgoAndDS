022b3df1acd293052de8bc352e6352ea26efd517
Sidharth Nambiar <snambiar01@gmail.com>
2022-11-15 15:00:13 -0500

    completed exercise 8

--- sections/06/my-minSubArrayLen.js
+++ sections/06/my-minSubArrayLen.js
@@ -6,14 +6,50 @@
 
 // Space Complexity - O(1)
 
+function minSubArrayLen(positiveArr, positiveNum) {
+  let minLength = Infinity;
 
-function minSubArrayLen(poitiveArr, positiveNum) {
-  
+  let sum = 0;
+  let startIndex = 0;
+  let incrementingIndex = 0;
+
+
+  while (startIndex < positiveArr.length) {
+    
+    if (sum < positiveNum) {
+      // console.log(startIndex, positiveArr[startIndex + incrementingIndex])
+      if (!positiveArr[startIndex + incrementingIndex]) break;
+      sum += positiveArr[startIndex+incrementingIndex]
+      incrementingIndex++;
+     
+    }
+    
+    if (sum >= positiveNum) {
+      // console.log("sum is:", sum)
+      // console.log("incrementer is:", incrementingIndex)
+      if (incrementingIndex <= minLength) {
+        minLength = incrementingIndex;
+      }
+      
+      startIndex++;
+      sum = 0;
+      incrementingIndex = 0
+      // console.log("min length:", minLength)
+    }
+    
+
+  }
+  // console.log("RESULT IS")
+  return minLength === Infinity ? 0 : minLength
+   
+
+ 
 }
 
-console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest console.log(subarray
-console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is the smallest console.log(subarray
-console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is console.log(greater than 52
+console.log(minSubArrayLen([1,1,1,1,1,1,1,1,1,2,5,1], 7)); //2  -> because [4,3] is the smallest
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is the smallest
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is
 console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
 console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
 console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
