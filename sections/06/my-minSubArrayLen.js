85e5724a5145a3434ef0360ada240b245db78472
Sidharth Nambiar <snambiar01@gmail.com>
2022-11-15 15:01:10 -0500

    removed console logs

--- sections/06/my-minSubArrayLen.js
+++ sections/06/my-minSubArrayLen.js
@@ -13,40 +13,27 @@ function minSubArrayLen(positiveArr, positiveNum) {
   let startIndex = 0;
   let incrementingIndex = 0;
 
-
   while (startIndex < positiveArr.length) {
-    
     if (sum < positiveNum) {
-      // console.log(startIndex, positiveArr[startIndex + incrementingIndex])
       if (!positiveArr[startIndex + incrementingIndex]) break;
-      sum += positiveArr[startIndex+incrementingIndex]
+      sum += positiveArr[startIndex + incrementingIndex];
       incrementingIndex++;
-     
     }
-    
+
     if (sum >= positiveNum) {
-      // console.log("sum is:", sum)
-      // console.log("incrementer is:", incrementingIndex)
       if (incrementingIndex <= minLength) {
         minLength = incrementingIndex;
       }
-      
+
       startIndex++;
       sum = 0;
-      incrementingIndex = 0
-      // console.log("min length:", minLength)
+      incrementingIndex = 0;
     }
-    
-
   }
-  // console.log("RESULT IS")
-  return minLength === Infinity ? 0 : minLength
-   
-
- 
+  return minLength === Infinity ? 0 : minLength;
 }
 
-console.log(minSubArrayLen([1,1,1,1,1,1,1,1,1,2,5,1], 7)); //2  -> because [4,3] is the smallest
+console.log(minSubArrayLen([1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 5, 1], 7)); //2  -> because [4,3] is the smallest
 console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest
 console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is the smallest
 console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is
